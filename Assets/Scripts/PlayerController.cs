91509457d3f5fa48c98820ebf64949abc0791cd9
Stanko2 <stanko@bezak.net>
2022-10-30 11:29:44 +0100

    Colliders For Map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        Application.targetFrameRate = 300;
     }
 
     private void FixedUpdate()
