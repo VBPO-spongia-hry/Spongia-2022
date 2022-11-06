b38ffa53c240d555b409474f94bd40c1737228a6
Stanko2 <stanko@bezak.net>
2022-11-06 15:45:04 +0100

    lights on

--- Assets/Scripts/MapArea.cs
+++ Assets/Scripts/MapArea.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
+[RequireComponent(typeof(PolygonCollider2D), typeof(Light2D))]
 public class MapArea : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Tower.Tower.OnTowerUpdated += ()
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Unlock()
     {
-        
+
+        // GetComponent<Light2D>().shapePath
+
     }
 }
