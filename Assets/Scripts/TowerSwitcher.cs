7e1810e00f51ec05a9af3a1919e2abe6787914ca
Stanko2 <stanko@bezak.net>
2022-11-06 20:10:29 +0100

    tower on map

--- Assets/Scripts/TowerSwitcher.cs
+++ Assets/Scripts/TowerSwitcher.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(HintObject))]
 public class TowerSwitcher : MonoBehaviour
 {
+    public Sprite[] TowerLevels;
     private HintObject _hint;
+    private SpriteRenderer _renderer;
     private void Start()
     {
         _hint = GetComponent<HintObject>();
+        _renderer = GetComponentInChildren<SpriteRenderer>();
     }
     private void Update()
     {
@@ -16,6 +19,19 @@ public class TowerSwitcher : MonoBehaviour
         {
             ShowTower();
         }
+    }
+
+    private void OnEnable()
+    {
+        if (Tower.Tower.Instance != null && Tower.Tower.Instance.level < TowerLevels.Length)
+            SetSprite();
+    }
+
+    private void SetSprite()
+    {
+        Destroy(_renderer.GetComponent<PolygonCollider2D>());
+        _renderer.sprite = TowerLevels[Tower.Tower.Instance.level - 1];
+        _renderer.gameObject.AddComponent<PolygonCollider2D>();
     }
 
     private void ShowTower()
